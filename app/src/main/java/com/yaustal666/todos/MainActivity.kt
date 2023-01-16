64c59f1af3f2406468d01e7923593989783efb92
yaustal666 <max.poz@mail.ru>
2023-01-16 12:18:02 +1000

    initial comm

---
+++ app/src/main/java/com/yaustal666/todos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yaustal666.todos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
